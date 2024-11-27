fa033819354d1956f86d97ec272ed4b1f2406a6b
shwetaTh <shwetaTh@users.noreply.github.com>
2024-11-27 15:11:04 +0545

    testing routes

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -3,12 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Testing</title>
 </head>
 <body>
-    <H2>hello world</H2>
-    <a href="/testing">Click me</a>
-    <a href="/home">Home</a>
-    <a href="/login">login</a>
+    <h1>This is route testing</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/testing', function () {
+    return view('testing');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
